0417d1c6959f5a042c42513fc8d39a8695c64a4e
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2020-03-26 15:30:28 +0000

    Added asset image and scaffold body

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,15 @@ import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
     home: Scaffold(
+      backgroundColor: Colors.blueGrey,
       appBar: AppBar(
         backgroundColor: Colors.blueGrey[900],
         title: Text('I am rich'),
+      ),
+      body: Center(
+        child: Image(
+          image: AssetImage('images/diamond.png'),
+        ),
       ),
     ),
   ),
